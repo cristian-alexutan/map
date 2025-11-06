f0e09aa2537bd4e611bb0f7a1753b32bb7157e78
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2025-11-06 18:54:47 +0200

    added all 3 working examples

--- src/Main.java
+++ src/Main.java
@@ -27,4 +27,45 @@ void main() {
     } catch (Exception e) {
         System.out.println(e.getMessage());
     }
+
+    // int a;int b; a=2+3*5;b=a+1;Print(b)
+    Statement ex2 = new CompoundStatement( new DeclarationStatement("a",new IntType()),
+new CompoundStatement(new DeclarationStatement("b",new IntType()),
+new CompoundStatement(new AssignStatement("a", new ArithmeticExpression(new ValueExpression(new IntValue(2)),new
+ArithmeticExpression(new ValueExpression(new IntValue(3)), new ValueExpression(new IntValue(5)), '*'), '+')),
+ new CompoundStatement(new AssignStatement("b",new ArithmeticExpression(new VariableExpression("a"), new ValueExpression(new
+IntValue(1)), '+')), new PrintStatement(new VariableExpression("b"))))));
+
+    IStack<Statement> exeStack2 = new MochaStack<>();
+    IDictionary<String, Value> symTable2 = new MochaDictionary<>();
+    IList<Value> out2 = new MochaList<>();
+    exeStack2.push(ex2);
+    ProgramState programState2 = new ProgramState(exeStack2, symTable2, out2);
+    IRepository repository2 = new Repository(programState2);
+    Controller controller2 = new Controller(repository2, true);
+    try {
+        controller2.allSteps();
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+    // bool a; int v; a=true;(If a Then v=2 Else v=3);Print(v)
+    Statement ex3 = new CompoundStatement(new DeclarationStatement("a",new BoolType()),
+ new CompoundStatement(new DeclarationStatement("v", new IntType()),
+new CompoundStatement(new AssignStatement("a", new ValueExpression(new BoolValue(true))),
+ new CompoundStatement(new IfStatement(new VariableExpression("a"),new AssignStatement("v",new ValueExpression(new
+IntValue(2))), new AssignStatement("v", new ValueExpression(new IntValue(3)))), new PrintStatement(new
+VariableExpression("v"))))));
+    IStack<Statement> exeStack3 = new MochaStack<>();
+    IDictionary<String, Value> symTable3 = new MochaDictionary<>();
+    IList<Value> out3 = new MochaList<>();
+    exeStack3.push(ex3);
+    ProgramState programState3 = new ProgramState(exeStack3, symTable3, out3);
+    IRepository repository3 = new Repository(programState3);
+    Controller controller3 = new Controller(repository3, true);
+    try {
+        controller3.allSteps();
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
 }
