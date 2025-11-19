b695f7aad7e108e6a5bbb8c67f427ba16a04fde1
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2025-11-19 20:30:55 +0200

    made mochastack use LinkedList

--- src/model/container/MochaStack.java
+++ src/model/container/MochaStack.java
@@ -2,54 +2,55 @@ package model.container;
 
 import exceptions.MochaStackException;
 
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class MochaStack<T> implements IStack<T> {
-    Stack<T> stack;
+    private LinkedList<T> list;
 
     public MochaStack() {
-        stack = new Stack<>();
+        list = new LinkedList<>();
     }
 
     @Override
     public void push(T val) {
-        stack.push(val);
+        list.addFirst(val);
     }
 
     @Override
     public T pop() throws MochaStackException {
-        if (stack.isEmpty()) throw new MochaStackException("Stack is empty");
-        return stack.pop();
+        if (list.isEmpty()) throw new MochaStackException("Stack is empty");
+        return list.removeFirst();
     }
 
     @Override
     public T top() throws MochaStackException {
-        if (stack.isEmpty()) throw new MochaStackException("Stack is empty");
-        return stack.peek();
+        if (list.isEmpty()) throw new MochaStackException("Stack is empty");
+        return list.peekFirst();
     }
 
     @Override
     public boolean isEmpty() {
-        return stack.isEmpty();
+        return list.isEmpty();
     }
 
     @Override
     public String toString() {
         StringBuilder ans = new StringBuilder();
-        for (T val : stack)
-            ans.insert(0, val.toString() + "\n");
+        for (T val : list) {
+            ans.append(val.toString()).append("\n");
+        }
         return ans.toString();
     }
 
     @Override
     public IStack<T> deepCopy() {
         MochaStack<T> newStack = new MochaStack<>();
-        Stack<T> tempStack = new Stack<>();
-        for (T val : stack) {
-            tempStack.push(val);
+        LinkedList<T> tempList = new LinkedList<>();
+        for (T val : list) {
+            tempList.addFirst(val);
         }
-        while (!tempStack.isEmpty()) {
-            newStack.push(tempStack.pop());
+        for (T val : tempList) {
+            newStack.push(val);
         }
         return newStack;
     }
